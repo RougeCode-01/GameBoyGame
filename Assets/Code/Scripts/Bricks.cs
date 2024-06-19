6ab279457c79ac503ac262a0ec887569b05c6e64
Christian Rojas <koenigrojass@gmail.com>
2024-06-19 11:16:41 -0600

    Organizing the folders

---
+++ Assets/Code/Scripts/Bricks.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bricks : MonoBehaviour
+{
+    private int bricksHealth = 2; // Health of the brick
+    private int points = 100;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if (bricksHealth == 1)
+        {
+            _spriteRenderer.color = Color.red;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            bricksHealth--;
+            if (bricksHealth == 0)
+            {
+                OnDestroy();
+            }
+        }
+    }
+    private void OnDestroy()
+    {
+        Destroy(gameObject);
+    }
+}
