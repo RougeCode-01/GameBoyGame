45aace53c524229163eb047f849e7dc1e4fab4ab
safdfadf <maahuja123@gmail.com>
2024-06-20 06:38:34 -0600

    Level1 Walls
    3 different types of wall

---
+++ Assets/Code/Scripts/Wall/Destroy_Brick.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_Brick : MonoBehaviour
+{
+    // Number of hits required to break the brick
+    public int hitPoints = 1;
+
+    private int currentHits = 0;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            currentHits++;
+            if (currentHits >= hitPoints)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
