9891d9b2043bf434e58ddb3b264843c361d9e0c4
Christian Rojas <koenigrojass@gmail.com>
2024-06-20 11:16:40 -0600

    Main Menu now works with controller

--- Assets/Code/Scripts/UI/MainMenu.cs
+++ Assets/Code/Scripts/UI/MainMenu.cs
@@ -1,10 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GBTemplate;
 
 public class MainMenu : MonoBehaviour
 {
+    private GBConsoleController _gb;
+
+    private void Awake()
+    {
+        _gb = GBConsoleController.GetInstance();
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Level 1");
@@ -12,5 +21,17 @@ public class MainMenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    private void Update()
+    {
+        if(_gb.Input.ButtonStartJustPressed)
+        {
+            StartGame();
+        }
+        if(_gb.Input.ButtonSelectJustPressed)
+        {
+            Quit();
+        }
     }
 }
